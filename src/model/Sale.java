9ae05e704a81457dd56684461569df6aed4e0e25
Alex Gainza Homar <alexgainza12@gmail.com>
2024-05-19 18:20:14 +0200

    Primer commit

--- src/model/Sale.java
+++ src/model/Sale.java
@@ -1,46 +1,72 @@
 package model;
 
-import java.util.Arrays;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sale {
-	String client;
-	Product[] products;
-	double amount;
-	
-	
-	public Sale(String client, Product[] products, double amount) {
-		super();
-		this.client = client;
-		this.products = products;
-		this.amount = amount;
-	}
-	
-	public String getClient() {
-		return client;
-	}
-	public void setClient(String client) {
-		this.client = client;
-	}
-	public Product[] getProducts() {
-		return products;
-	}
-	public void setProducts(Product[] products) {
-		this.products = products;
-	}
-	public double getAmount() {
-		return amount;
-	}
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
+    Client client;
+    List<Product> products;
+    double amount;
+    LocalDateTime saleDateTime;
 
-	@Override
-	public String toString() {
-		return "Sale [client=" + client + ", products=" + Arrays.toString(products) + ", amount=" + amount + "]";
-	}
-	
-	
-	
-	
+    public Sale(Client client, ArrayList<Product> products, double totalAmount, LocalDateTime saleDateTime) {
+        this.client = client;
+        this.products = products;
+        this.amount = totalAmount;
+        this.saleDateTime = saleDateTime; // Asegúrate de que saleDateTime se inicializa correctamente aquí
+    }
 
+	public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getSaleDateTime() {
+        return saleDateTime;
+    }
+
+    public void setSaleDateTime(LocalDateTime saleDateTime) {
+        this.saleDateTime = saleDateTime;
+    }
+
+    public String getSaleDateTimeFormatted() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return saleDateTime.format(formatter);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Sale{" +
+                "client='" + client + '\'' +
+                ", products=" + products +
+                ", amount=" + amount +
+                ", saleDateTime=" + saleDateTime +
+                '}';
+    }
+
+    public double getTotalAmount() {
+        return amount;
+    }
 }
